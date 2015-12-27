604175c87537e459e64d429d9c029cd27cd3f8eb
José Queirós <zekaroz@gmail.com>
2015-12-27 22:06:51 +0000

    Shop table and My Sql Configured

--- Opel/database/migrations/2015_12_27_183732_CreateShop.php
+++ Opel/database/migrations/2015_12_27_183732_CreateShop.php
@@ -12,7 +12,7 @@ class CreateShop extends Migration
      */
     public function up()
     {
-        Schema::create('shop', function (Blueprint $table) {
+        Schema::create('shops', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('shopDescription');
