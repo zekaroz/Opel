8a8a007af55cd9a7f54e3136bb566051a47d35c0
José Queirós <zekaroz@gmail.com>
2016-01-02 13:02:21 +0000

    List, Show and Create Working

--- Opel/resources/views/layouts/dashboard.blade.php
+++ Opel/resources/views/layouts/dashboard.blade.php
@@ -9,9 +9,41 @@
                 <a class="navbar-brand" href="{{ url ('') }}">Reci-Opel | Laravel 5</a>
             </div>
             <div class="navbar-header">
+            <!-- /.navbar-top-links -->
             </div>
           <!--    /.navbar-top-links -->
+<!-- user profile -->   
+                @if(! Auth::check() )
+                    <div class="navbar-header" style="margin-left: 35%;margin-top: 9px;">
+                        <a href="auth/login" class="btn btn-success btn-outline" style="width: 250px">Log In</a>
+                    </div>
+                @else
+                    <ul class="nav navbar-top-links navbar-right">
+                        <!-- /.dropdown -->
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-user">
+                                <ul style="padding:0px;">
+                                        <li><a href="#"><i class="fa fa-user fa-fw"></i> {{Auth::user()->name}}  </a>
+                                        </li>
+                                    <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                                    </li>
+                                    <li class="divider"></li>
+                                <li><a href="{{url('/')}}/auth/logout">Logout</a></li>
+                                </ul>
+                                <!-- /.dropdown-user -->    
+                            </div>
 
+                        </li>
+                        <!-- /.dropdown -->
+                    </ul>
+                @endif
+          
+          
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
