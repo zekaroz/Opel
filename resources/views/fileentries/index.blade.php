4a4eacbe920a907a4eb1a93fba2be814088b6d23
José Queirós <zekaroz@gmail.com>
2016-02-09 15:20:27 +0000

    Dropzone JS implementado em página de testes

---
+++ resources/views/fileentries/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.dashboard')
+
+@section('page_heading')
+Pictures Uploaded
+
+ @stop
+
+@section('section')
+       
+<div class="secure">Drop Zone form</div>
+  {!! Form::open(['url'=>'apply/upload','method'=>'POST', 'files'=>true, 'id'=>'myDropZone','class'=>'dropzone']) !!}
+
+{!! Form::close() !!}
+</div>
+      
+      
+ <h3> Pictures list</h3>
+ <div class="row">
+        <ul class="thumbnails">
+ @forelse($entries as $entry)
+            <div class="col-md-2">
+                <div class="thumbnail">
+                    <img src="{{route('getentry', $entry->filename)}}" alt="ALT NAME" class="img-responsive" />
+                    <div class="caption">
+                        <p>{{$entry->original_filename}}</p>
+                    </div>
+                </div>
+            </div>
+  @empty
+        <div>No images to show...</div>
+ @endforelse
+ 
+ </ul>
+ </div>
+
+    </ul>
+ </div>
+ 
+@stop
