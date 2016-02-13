d24a1a498b64a9d4808bc5a361cc330cf98489bf
José Queirós <zekaroz@gmail.com>
2016-02-13 21:17:57 +0000

    Teste do Github

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Bem Vindo</div>
+                <div class="panel-heading">Bem Vindo à ReciOpel</div>
 
                 <div class="panel-body">
-                    Bem vindo à loja!
+                    Bem vindo à loja onde encontra tudo relacionado com a marca Opel. 
                 </div>
             </div>
         </div>
