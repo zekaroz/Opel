0caaee2f0aa0ef6bcf915e753e29e23c624e0bb9
José Queirós <zekaroz@gmail.com>
2016-02-15 23:31:20 +0000

    Alterado o delete dos modelos para ter um metodo melhor de ajax submit. Agora o delete do modelo automáticamente remove a linha.

--- resources/views/backoffice/brands/_Models_table.blade.php
+++ resources/views/backoffice/brands/_Models_table.blade.php
@@ -1,20 +1,24 @@
 
 <script >
-    function destroyModel(modelid){
-        var postUrl = '/models/'+modelid;
-        
-        $.ajax({
-        url: postUrl,
-        type: 'post',
-        data: {_method: 'delete'},
-        success:function(msg) {
-                document.location.reload(true);
-            },
-         error:function(msg) {
-               alert('Cant Delete');  
-            }
+    $(document).ready( function( $ ) {        
+        $( '.deleteLink' ).on( 'click', function(e) {
+                    e.preventDefault();
+                    var link = $(this);
+                    var postUrl = '/models/'+link.attr('data-id');
+
+                    $.ajax({
+                        url: postUrl,
+                        type: 'post',
+                        data: {_method: 'delete'},
+                        success:function(msg) {
+                            link.closest('tr').remove();
+                         },
+                        error:function(msg) {
+                           alert('Something wrong...');
+                        }
+                    });
         });
-    }
+    });
 </script>
 
 <table class="table table-striped">
@@ -33,8 +37,8 @@
                    {{ $model->code}}
                 </td>
                 <td>              
-                    <div>
-                        <a  href="javascript:destroyModel({{$model->id}});">
+                    <div style="text-align: right;">
+                        <a  href="#" class="deleteLink btn btn-default" data-id="{{$model->id}}">
                             <span><i class="fa fa-trash-o fa-fw"></i>  </span>
                         </a>
                     </div>
