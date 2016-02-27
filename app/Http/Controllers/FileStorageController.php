21af5a4ff467f8c1c89cd93e15684751fee4da3d
José Queirós <zekaroz@gmail.com>
2016-02-27 22:40:38 +0000

    Corrigido o problema dos Thumbnails. Alterada a forma de guardar os binários para usar sempre o Storage lib. Fix no master e ready para fazer delete do resto dos Branches por causa deste problema.

--- app/Http/Controllers/FileStorageController.php
+++ app/Http/Controllers/FileStorageController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\File;
@@ -15,31 +14,32 @@ class FileStorageController extends Controller
     public function __construct() {
 
     }
-    
+
     public function saveImage($path, $filename, $file) {        
-        
+
         $image = File::get($file);
-        
+
         $fullpath = $path.$filename;
-        
+
         Storage::disk('local')->put($fullpath, $image );
-       
+
         return $fullpath;
     }
-    
+
     public function saveThumbnail($path, $filename,  $file,  $width,  $height) {
         $fullpath = $path.$filename;
         // create an image
-        //Image::make($file->getRealPath())->fit($width, $height)->save(public_path($fullpath));
-        
+        $image = Image::make($file->getRealPath())->fit($width, $height);
+
+        Storage::disk('local')->put($fullpath, $image->stream() );
+
         return $fullpath;
     }
-    
+
     public function getImage(string $path) {
         $file = new File();
-        
+
         return $file;
     }
 }
 
- 
