27d1f797b0e222f85c0f154c068be20da52d37e0
José Queirós <zekaroz@gmail.com>
2016-11-22 21:24:17 +0000

    melhorado o Styling do sidemenu com as pesquisas possíveis.

--- resources/views/online_shop/layouts/sidemenu.blade.php
+++ resources/views/online_shop/layouts/sidemenu.blade.php
@@ -1,17 +1,18 @@
 <p class="lead">A nossa Oferta:</p>
 <div class="list-group">
-    <a href="/pecas" class="list-group-item">
-        <i class="fa fa-cogs"></i>
-        <span >
-           Peças recicladas
-        </span>    </a>
-    <a href="/carros" class="list-group-item">
+    <a href="/pecas" class="list-group-item {{ Request::path() == 'pecas' ? 'page-selected' : '' }}">
+        <i class="fa fa-cogs "></i>
+        <span>
+             Peças recicladas
+        </span>
+    </a>
+    <a href="/carros" class="list-group-item {{ Request::path() == 'carros' ? 'page-selected' : '' }}">
         <i class="fa fa-car"></i>
         <span>
             Veículos usados
         </span>
     </a>
-    <a href="/carros_para_pecas" class="list-group-item">
+    <a href="/carros_para_pecas" class="list-group-item {{ Request::path() == 'carros_para_pecas' ? 'page-selected' : '' }}">
         <i class="fa fa-wrench"></i>
         <span>
             Veículos para Peças
