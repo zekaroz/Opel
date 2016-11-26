10e9629d5be1f561ab380c3495157b385e0fae32
José Queirós <zekaroz@gmail.com>
2016-11-26 12:22:23 +0000

    Imagens agora têm a orientação correcta.

--- app/Http/Controllers/FileEntryController.php
+++ app/Http/Controllers/FileEntryController.php
@@ -12,11 +12,11 @@ use App\Http\Controllers\FileStorageController;
 
 class FileEntryController extends Controller
 {
-    
+
         public function __construct() {
-           
+
         }
-    
+
 	/**
 	 * Display a listing of the resource.
 	 *
@@ -25,57 +25,54 @@ class FileEntryController extends Controller
 	public function index()
 	{
 		$entries = Fileentry::all();
- 
+
 		return view('fileentries.index', compact('entries'));
 	}
-  
+
 	public function add() {
-            
+
                 $file = Request::file('file');
 		$extension = $file->getClientOriginalExtension();
-		
+
                 Storage::disk('local')->put($file->getFilename().'.'.$extension,  File::get($file));
-		
+
                 $entry = new Fileentry();
 		$entry->mime = $file->getClientMimeType();
 		$entry->original_filename = $file->getClientOriginalName();
 		$entry->filename = $file->getFilename().'.'.$extension;
                 $entry->path = $file->getFilename().'.'.$extension;
-                
+
 		$entry->save();
- 
+
 		return redirect('fileentry');
-		
+
 	}
-        
+
 	public function get($filename){
 		$entry = Fileentry::where('filename', '=', $filename)->firstOrFail();
-                $fs = new FileStorageController();
-                $file = $fs->getImage($entry->path);
+    $fs = new FileStorageController();
+    $file = $fs->getImage($entry->path);
 		return (new Response($file, 200))
                     ->header('Content-Type', $entry->mime);
 	}
-	
-        public function getThumbnail($filename){
 
+  public function getThumbnail($filename){
 		$entry = Fileentry::where('filename', '=', $filename)->firstOrFail();
-                $fs = new FileStorageController();
-                $file = $fs->getImage($entry->thumbnail_path);
-		return (new Response($file, 200))
-                    ->header('Content-Type', $entry->mime);
+    $fs = new FileStorageController();
+    $file = $fs->getImage($entry->thumbnail_path);
+
+    return (new Response($file, 200))
+                  ->header('Content-Type', $entry->mime);
 	}
-        
-        
-        public function destroy($file_id) {
-            
-            $file = Fileentry::findOrFail($file_id);
 
-            $file->delete();
 
-            return \Response::json([
-                               'error' => false,
-                               'code'  => 200, 
-                               'feedback' =>'Model has been deleted.'
-                           ], 200);        
-        }
+    public function destroy($file_id) {
+        $file = Fileentry::findOrFail($file_id);
+        $file->delete();
+        return \Response::json([
+                           'error' => false,
+                           'code'  => 200,
+                           'feedback' =>'Model has been deleted.'
+                       ], 200);
+    }
 }
