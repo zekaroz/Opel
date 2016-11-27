f9ea70fa6c64310d1263909e3b4fe9be520a20f2
José Queirós <zekaroz@gmail.com>
2016-11-27 23:49:11 +0000

    Adicionei uma slug à tabela de artigos, na migration fiz o update dos artigos actuais. No save gravamos agora uma slug. no Sitemap do site agora os artigos são descritos com uma slug para serem melhor apanhados pelo google.

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -165,6 +165,8 @@ class ArticlesController extends Controller
            $article->part_type_id = null;
         }
 
+        $article->slug = str_slug($article->name) .'-' .$article->id;
+
         // this automatically applies the user id for
         //the relations ship
         //TODO: rever isto para associar a peça à loja de que o user é dono;
