32b4287951fb7038d9492b3c953f8d0aa8d7ef5e
José Queirós <zekaroz@gmail.com>
2016-12-12 19:33:13 +0000

    Bootstrap styles estavam duplicados.

--- resources/views/dashboards/index.blade.php
+++ resources/views/dashboards/index.blade.php
@@ -12,22 +12,28 @@ Dashboard
 
 <div class="container">
   <div class="row">
-      <div class="col-sm-6 col-xs-12" >
+      <div class="col-sm-5  col-xs-12 thumbnail" style="margin-top: 20px;">
+        <div class="">
+              Artigos por Tipo de peça
+        </div>
         <canvas id="PieChart" style=""></canvas>
         <div id="js-legend-pie_PieChart" class="chart-legend"></div>
       </div>
-      <div class="col-sm-6 col-xs-12">
+      <div class="col-sm-5 col-offset-sm-2 col-xs-12 thumbnail" style="margin-top: 20px;">
+        <div class="">
+              Artigos por marca
+        </div>
         <canvas id="PieChart2" style=""></canvas>
         <div id="js-legend-pie_PieChart2" class="chart-legend"></div>
       </div>
   </div>
 
   <div class="row">
-    <div class="col-sm-6 col-xs-12">
+    <div class="col-sm-5 col-xs-12 thumbnail" style="margin-top: 40px;">
       <canvas id="BarChart"></canvas>
       <div id="js-legend-bar_BarChart" class="chart-legend"></div>
     </div>
-    <div class="col-sm-6 col-xs-12">
+    <div class="col-sm-5 col-xs-12 thumbnail" style="margin-top: 40px;">
       <canvas id="LineChart" style="width:50%;"></canvas>
       <div id="js-legend-line_LineChart" class="chart-legend"></div>
     </div>
