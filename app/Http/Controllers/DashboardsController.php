a12547295e952e7a9445ea4cff964145a1f86ab2
José Queirós <zekaroz@gmail.com>
2016-12-13 23:07:45 +0000

    Adicionado o gráfico dos artigos criados por Ano e mês

--- app/Http/Controllers/DashboardsController.php
+++ app/Http/Controllers/DashboardsController.php
@@ -51,14 +51,44 @@ class DashboardsController extends Controller
                                      ->groupBy('partType')
                                      ->pluck('article_price', 'partType');
 
-
         $data_series = collect($dataPartTypes_by_price)->map(function($x){ return  (array) $x ; })->toArray();
 
+        $articlesByYear_dataseries = DB::select("SELECT Months.month, count(distinct articles.id) as value
+              FROM
+                  (SELECT DATE_FORMAT(now(), '%m/%y') AS Month, 12 as mes, year(now()) as ano
+                   UNION  SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 1 MONTH), '%m/%y'), 11 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 2 MONTH), '%m/%y'), 10 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 3 MONTH), '%m/%y'), 9 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 4 MONTH), '%m/%y'), 8 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 5 MONTH), '%m/%y'), 7 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 6 MONTH), '%m/%y'), 6 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 7 MONTH), '%m/%y'), 5 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 8 MONTH), '%m/%y'), 4 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 9 MONTH), '%m/%y'), 3 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 10 MONTH), '%m/%y'), 2 as mes, year(now()) as ano
+                   UNION   SELECT DATE_FORMAT(DATE_SUB(now(), INTERVAL 11 MONTH), '%m/%y'), 1 as mes, year(now()) as ano
+                  ) AS Months
+              left join articles
+                on year(articles.created_at) = Months.ano
+                  and month(articles.created_at) = Months.mes
+              GROUP BY Months.month
+              ORDER BY 1");
 
+       $newArticleList = array();
+         collect($articlesByYear_dataseries)->map(function($item) use(&$newArticleList) {
+            $newArticleList[$item->month] = $item->value;
+        });
+
+        $articlesByYear_dataseries = $newArticleList;
+
+        $articlesByYear_dataseries = collect($articlesByYear_dataseries)->map(function($value){
+                                                                    return (array)  $value;
+                                                                })->toArray();
         return view('dashboards.index')
                     ->with(compact('data'))
                     ->with(compact('data_series'))
                     ->with(compact('dataPartTypes'))
-                    ->with(compact('dataPartTypes_by_price'));
+                    ->with(compact('dataPartTypes_by_price'))
+                    ->with(compact('articlesByYear_dataseries'));
     }
 }
