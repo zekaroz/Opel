34a16f9ee554737b512630fe9ea664751674d233
José Queirós <zekaroz@gmail.com>
2016-12-15 00:48:17 +0000

    Melhorados os gráficos. Melhorada a página de entrada.

--- config/chartjs.php
+++ config/chartjs.php
@@ -38,94 +38,21 @@ return [
          * the data will use default color definied in ChartPieAndDougnhut.php
          */
         'pie' => [
-            [
-                'colour' => "#F7464A",
-                'highlight' => "#FF5A5E"
-            ],
-            [
-                'colour' => "#46BFBD",
-                'highlight' => "#5AD3D1"
-            ],
-            [
-                'colour' => "#FDB45C",
-                'highlight' => "#FFC870"
-            ],
-            [
-                'colour' => "#ffd700",
-                'highlight' => "#ffd900"
-            ],
-            [
-                'colour' => "#464130",
-                'highlight' => "#474337"
-            ],
-            [
-                'colour' => "#a8b17a",
-                'highlight' => "#b8c19a"
-            ],
-            [
-                'colour' => "#281b7b",
-                'highlight' => "#383b8b"
-            ],
-            [
-                'colour' => "#234761",
-                'highlight' => "#335771"
-            ],
-            [
-                'colour' => "#0a4f35",
-                'highlight' => "#1a5f45"
-            ],
-            [
-                'colour' => "#cdf3ac",
-                'highlight' => "#Ddf9bc"
-            ],
-            [
-                'colour' => "#4a8286",
-                'highlight' => "#5a9296"
-            ],
-            [
-                'colour' => "#F7464A",
-                'highlight' => "#FF5A5E"
-            ],
-            [
-                'colour' => "#46BFBD",
-                'highlight' => "#5AD3D1"
-            ],
-            [
-                'colour' => "#FDB45C",
-                'highlight' => "#FFC870"
-            ],
-            [
-                'colour' => "#ffd700",
-                'highlight' => "#ffd900"
-            ],
-            [
-                'colour' => "#464130",
-                'highlight' => "#474337"
-            ],
-            [
-                'colour' => "#a8b17a",
-                'highlight' => "#b8c19a"
-            ],
-            [
-                'colour' => "#281b7b",
-                'highlight' => "#383b8b"
-            ],
-            [
-                'colour' => "#234761",
-                'highlight' => "#335771"
-            ],
-            [
-                'colour' => "#0a4f35",
-                'highlight' => "#1a5f45"
-            ],
-            [
-                'colour' => "#cdf3ac",
-                'highlight' => "#Ddf9bc"
-            ],
-            [
-                'colour' => "#4a8286",
-                'highlight' => "#5a9296"
-            ]
+            [ 'colour' => "#F7464A",  'highlight' => "#FF5A5E" ],
+            [ 'colour' => "#46BFBD",  'highlight' => "#5AD3D1" ],
+            [ 'colour' => "#FDB45C",  'highlight' => "#FFC870" ],
+            [ 'colour' => "#ffd700",  'highlight' => "#ffd900" ],
+            [ 'colour' => "#a8b17a",  'highlight' => "#b8c19a" ],
+            [ 'colour' => "#FF4500",  'highlight' => "#383b8b" ],
+            [ 'colour' => "#4169E1",  'highlight' => "#335771" ],
+            [ 'colour' => "#2E8B57",  'highlight' => "#1a5f45" ],
+            [ 'colour' => "#D2B48C",  'highlight' => "#Ddf9bc" ],
+            [ 'colour' => "#FF6347",  'highlight' => "#5a9296" ],
+            [ 'colour' => "#B0C4DE",  'highlight' => "#383b8b" ],
+            [ 'colour' => "#87CEFA",  'highlight' => "#335771" ],
+            [ 'colour' => "#FFA07A",  'highlight' => "#1a5f45" ],
+            [ 'colour' => "#DAA520",  'highlight' => "#Ddf9bc" ],
+            [ 'colour' => "#228B22",  'highlight' => "#5a9296" ]
         ]
     ],
 
